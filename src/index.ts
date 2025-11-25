3f42073556ec6709308b6558202504394ec755aa
taeyoung24 <jtyoung24@naver.com>
2025-11-25 21:23:38 +0900

    Add Swagger documentation setup and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
+import swaggerJSDoc from 'swagger-ui-express';
+import { swaggerSpec } from './config/swagger';
 import {
   apiRouter,
   indexRouter,
@@ -15,6 +17,9 @@ app.use('/api', apiRouter);
 
 // 들어오는 JSON 요청 body를 자동으로 파싱해서 req.body에 넣어주는 미들웨어 등록
 app.use(express.json()); 
+
+// Swagger UI 설정
+app.use('/api-docs', swaggerJSDoc.serve, swaggerJSDoc.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
